9fb2aaaeadc9beba38e5570e790d769bb5e6e2cd
Prapakorn <prapakornpm2001@gmail.com>
2024-05-14 12:43:50 +0700

    Rewrite for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from '@/pages/home';
 import DetailPage from '@/pages/detail';
